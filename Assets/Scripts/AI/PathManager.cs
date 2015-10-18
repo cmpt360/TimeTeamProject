650e799f91f784b287be3b2acb5a81c8d53d8268
Michael Butler <kaptainkoloth@gmail.com>
2015-10-18 00:23:29 -0600

    AI Code
    Added the AIManager
    Added the PathManager
    Changed stats config (had to change it from a struct to get it working
    right)
    Enemies now generate random stats with a min/max stat for a certain
    weight, and move/angle towards the player.
    Now working on AI types.

---
+++ Assets/Scripts/AI/PathManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathManager : MonoBehaviour {
+
+	bool obstacle;
+	bool pause = false;
+	Vector3 player;
+	public float mobSp;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		PathToTarget ("Player");
+	
+	}
+
+	//Point towards and move towards target. If theres a collision with a map object, walk a random direction and try again.
+	//Tags in the string of the tag of the object to path towards
+	//TODO Target picking logic if theres more then one entity with that tag.
+	public void PathToTarget(string tag){
+		player = GameObject.FindWithTag (tag).transform.localPosition;
+		if (!obstacle) {
+			float rotZ = Mathf.Atan2 (player.y - transform.position.y, player.x - transform.position.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis (rotZ, Vector3.forward);
+		} else if (obstacle && !pause) {
+			//print ("Going random angle");
+			float randomAng = Random.Range (-90.0f, 90.0f);
+			//print ("I picked: " + randomAng + "!");
+			transform.rotation = Quaternion.AngleAxis (Random.Range (-90.0f, 90.0f), Vector3.forward);
+			pause = true;
+		}
+		//print ("moving forward");
+		transform.Translate (Vector3.right * mobSp * Time.deltaTime);
+	}
+
+	//Wait to allow object to move away from obstacle (hopefully). Then try and resume following player.
+	IEnumerator waitForMove()
+	{
+		//print ("waiting to change direction");
+		obstacle = true;
+		pause = false;
+		yield return new WaitForSeconds(4);
+		obstacle = false;
+		//print ("finished changing direction");
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		//TODO Tags need to be adjusted for all collidables.
+		if (collision.gameObject.tag == "Wall") {
+			StartCoroutine(waitForMove ());
+		}
+	}
+
+}
--- Assets/Scripts/StatCollectionClass.cs
+++ Assets/Scripts/StatCollectionClass.cs
@@ -2,38 +2,18 @@
 using System.Collections;
 
 public class StatCollectionClass : MonoBehaviour {
+	
+	public int health;
+	
+	public int mana;
+		
+	public int strength;
+		
+	public int intellect;
+		
+	public int xp;
+		
+	public int playerLevel;
 
-	//private int[] stats = new int[20];
-	public struct StatCollection{
-		public int health{
-			get{return health;}
-			set{health = value;}
-		}
-
-		public int mana{
-			get{return mana;}
-			set{mana = value;}
-		}
-
-		public int strength{
-			get{return strength;}
-			set{strength = value;}
-		}
-
-		public int intellect{
-			get{return intellect;}
-			set{intellect = value;}
-		}
-
-		public int xp{
-			get{return xp;}
-			set{xp = value;}
-		}
-
-		public int playerLevel{
-			get{return playerLevel;}
-			set{playerLevel = value;}
-		}
-	}
 }
 
