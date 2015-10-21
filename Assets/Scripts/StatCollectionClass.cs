e9cc9c300c56aafbe854fc64a7ac8bfca11a2c49
Evilkitty1341 <Evilkitty1341@yahoo.com>
2015-10-21 17:57:58 -0600

    Added additional comments

--- Assets/Scripts/StatCollectionClass.cs
+++ Assets/Scripts/StatCollectionClass.cs
@@ -1,29 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// This class holds the various stats that will be used by the player and enemies in our game
+// Each object will have their own version of a stat collection
 public class StatCollectionClass : MonoBehaviour {
-
 
 	public float health;
 			
-		
-
 	public float mana;
 			
-
 	public int strength;
 			
-
 	public int intellect;
 			
-
 	public int xp;
 			
-
 	public int playerLevel;
 
 	public bool itemCoin;
-
 
 	public bool EnergyBallUnlocked;
 
@@ -32,7 +26,5 @@ public class StatCollectionClass : MonoBehaviour {
 	public bool FireBreathUnlocked;
 
 	public bool SunStrikeUnlocked;
-
-
 }
 
